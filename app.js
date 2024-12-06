a92122f8057af6fcf14e06b3e22864abc4cba324
YuHyeonjin <YHJ@hansung.ac.kr>
2024-12-06 12:25:35 +0900

    Update3 app.js file

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Nodejs Hello, World!\n');
+  res.end('2nd Hello world\n');
 });
 
 server.listen(port, hostname, () => {
